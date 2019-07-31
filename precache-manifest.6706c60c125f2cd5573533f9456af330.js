4d743bf37f6e50dae3346aad3a74d4762dd4a048
tobslob <kazmobileapp@gmail.com>
2019-07-31 15:34:14 +0100

    Updates

---
+++ precache-manifest.6706c60c125f2cd5573533f9456af330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2fd78657adc0c113bb418fb9628d2e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "351822431d667bcd625e",
+    "url": "/monsters-rolodex/static/css/main.36305114.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "351822431d667bcd625e",
+    "url": "/monsters-rolodex/static/js/main.d70a3972.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
