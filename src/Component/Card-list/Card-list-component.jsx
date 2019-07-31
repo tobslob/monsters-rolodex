21fa1f2628f64e242de932568ad021206d7fd262
tobslob <kazmobileapp@gmail.com>
2019-07-31 15:36:58 +0100

    add files for github pages

---
+++ src/Component/Card-list/Card-list-component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./card-list-style.css";
+
+import { Card } from '../Card/Card.component'
+
+const CardList = props => (
+  <div className="card-list">
+    {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster}/>
+    ))}
+  </div>
+);
+
+export default CardList;
